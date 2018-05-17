861027e25038a0f7983a99cf75302480c407af10
James Baker <jbaker1@mit.edu>
2018-05-17 19:59:10 +0800

    Create three endpoints - all_bets, place_bet, open_bets

--- bet.py
+++ bet.py
@@ -1,12 +1,15 @@
 from uuid import uuid4
 from flask import Flask, jsonify, request
+import time
 
 
 class Bettor:
-    def __init__(self, name):
-        self.uuid = uuid4()
+    def __init__(self, bettor_uuid, name):
+        self.uuid = bettor_uuid
         self.name = name
         self.bets = []
+    def __eq__(self, other):
+        return self.uuid == other.uuid
 
 
 class Spread:
@@ -24,13 +27,22 @@ class Bet:
         self.spread = spread
 
 class Game:
-    def __init__(self, home_team, away_team, end_time, home_team_score, away_team_score):
+    def __init__(self, home_team, away_team, end_time):
         self.end_time = end_time
         self.home_team = home_team
         self.away_team = away_team
         self.end_time = end_time
-        self.home_team_score = home_team_score
-        self.away_team_score = away_team_score
+        self.home_team_score = 0
+        self.away_team_score = 0
+    def result(self):
+        if self.end_time > time.now():
+            raise ValueError('Game not finished')
+        return Spread(self.home_team_score - self.away_team_score, self.home_team)
+    def __eq__(self, other):
+        return self.home_team == other.home_team and \
+               self.away_team == other.away_team and \
+               self.end_time == other.end_time
+
 
 class BettingSystem:
     def __init__(self):
@@ -46,27 +58,40 @@ class BettingSystem:
     def open_bets(self):
         return [bet for bet in self.bets if bet.bettee is None]
 
+    def get_game(self, home_team, away_team, end_time):
+        potential_new_game = Game(home_team, away_team, end_time)
+        for game in self.games:
+            if potential_new_game == game:
+                return game
+        self.games.append(potential_new_game)
+        return potential_new_game
 
+    def get_bettor(self, bettor_uuid, bettor_name):
+        potential_user = Bettor(bettor_uuid, bettor_name)
 
+        if bettor_uuid is None:
+            return None
 
-# print(test_betting.open_bets)
+        if potential_user not in self.users:
+            self.users.append(potential_user)
 
+        return potential_user
 
 
 app = Flask(__name__)
 
 game = BettingSystem()
-cavs = uuid4()
-celtics = uuid4()
-test_betting = BettingSystem()
-james = Bettor("James")
-jonny = Bettor("Jonny")
-test_spread = Spread(6, celtics)
-test_game = Game(cavs, celtics, 0, 0, 0)
-test_spread = Spread(6, celtics)
-test_open_bet = Bet(james, None, 10, test_game, test_spread)
+# cavs = uuid4()
+# celtics = uuid4()
+# test_betting = BettingSystem()
+# james = Bettor("James")
+# jonny = Bettor("Jonny")
+# test_spread = Spread(6, celtics)
+# test_game = Game(cavs, celtics, 0,)
+# test_spread = Spread(6, celtics)
+# test_open_bet = Bet(james, None, 10, test_game, test_spread)
 
-game.place_bet(test_open_bet)
+# game.place_bet(test_open_bet)
 
 @app.route("/open_bets", methods=['GET'])
 def open_bets():
@@ -85,11 +110,50 @@ def open_bets():
 
 @app.route("/place_bet", methods=['POST'])
 def place_bet():
-    required = ['bettor', 'bettee', 'amount', 'game']
+    values = request.get_json()
+
+    required = [
+                'bettor_uuid',
+                'bettor_name',
+                'amount',
+                'point_differential',
+                'spread_team',
+                'home_team',
+                'away_team',
+                'end_time'
+               ]
     if not all(k in values for k in required):
         return 'Missing values', 400
-    return 'Not done yet', 200
 
+    bettee_uuid = values['bettee_uuid'] if 'bettee_uuid' in values else None
+    bettee_name = values['bettee_name'] if 'bettee_name' in values else None
+
+    potential_game = game.get_game(values['home_team'], values['away_team'], values['end_time'])
+    bettor = game.get_bettor(values['bettor_uuid'], values['bettor_name'])
+    bettee = game.get_bettor(bettee_uuid, bettee_name)
+    spread = Spread(values['point_differential'], values['spread_team'])
+
+    new_bet = Bet(bettor, bettee, values['amount'], potential_game, spread)
+
+    game.place_bet(new_bet)
+
+    return 'Bet placed', 200
+
+@app.route("/all_bets", methods=['GET'])
+def all_bets():
+    open_bets_info = [
+                    {
+                    "bettor_name": bet.bettor.name,
+                    "bettee_name": "Open bet" if bet.bettee is None else bet.bettee.name,
+                    "amount": bet.amount,
+                    "home_team": bet.game.home_team,
+                    "away_team": bet.game.away_team,
+                    "spread_point_differential": bet.spread.point_differential,
+                    "spread_team": bet.spread.team
+                    }
+                    for bet in game.bets]
+
+    return jsonify(open_bets_info), 200
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
